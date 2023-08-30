b37ec6fc27fb28437fb4614c004b15d06b64cfd1
baruxu <baruxu@tencent.com>
2023-08-30 11:24:17 +0800

    make any hashable

--- Source/CommonUtils/Public/Any.h
+++ Source/CommonUtils/Public/Any.h
@@ -7,6 +7,9 @@
 #define UNIQUE_FUNCTION_ID __PRETTY_FUNCTION__
 #endif
 
+// determine FAny can be used as key in hashmap
+#define ENABLE_ANY_HASHABLE 1 
+
 template<class T>
 inline uint64 TTypeId()
 {
@@ -27,8 +30,8 @@ inline T AnyCast(const FAny&);
 class FAny
 {
 public:
-	FAny(){}
-	FAny(const FAny& Other): Holder(Other.Holder ? Other.Holder->Clone(): nullptr){}
+	FAny() {}
+	FAny(const FAny& Other) : Holder(Other.Holder ? Other.Holder->Clone() : nullptr) {}
 	FAny(FAny&& Other) : Holder(Other.Holder) { Other.Holder = nullptr; }
 
 
@@ -64,7 +67,7 @@ public:
 	FAny& operator= (typename TRemoveCVRef<ValueType>::Type Value)
 	{
 		FAny(MoveTemp(Value)).Swap(*this);
-		return *this; 
+		return *this;
 	}
 
 	template<class ValueType>
@@ -75,7 +78,7 @@ public:
 
 	uint64 Type()const
 	{
-		return Holder ? Holder->Type()  : 0;
+		return Holder ? Holder->Type() : 0;
 	}
 
 	bool IsNone()const
@@ -83,30 +86,55 @@ public:
 		return Holder == nullptr;
 	}
 
+#if ENABLE_ANY_HASHABLE
+
+	uint32 Hash()const
+	{
+		return Holder ? Holder->Hash() : 0;
+	}
+
+	bool operator ==(const FAny& Other)const
+	{
+		return  (!Holder && !Other.Holder) || ((Type() == Other.Type()) ? Holder->Equal(Other.Holder) : false);
+	}
+
+#endif
 public:
 	class PlaceHolderBase
 	{
 	public:
-		virtual ~PlaceHolderBase(){}
+		virtual ~PlaceHolderBase() {}
 
 		virtual uint64 Type()const = 0;
 		virtual PlaceHolderBase* Clone()const = 0;
+		virtual uint32 Hash()const = 0;
+		virtual bool Equal(const PlaceHolderBase* Other)const = 0;
 	};
 
 	template<class ValueType>
 	class PlaceHolder : public PlaceHolderBase
 	{
 	public:
-		PlaceHolder(ValueType InValue):
+		PlaceHolder(ValueType InValue) :
 			Value(MoveTemp(InValue))
 		{}
 
-		virtual uint64 Type()const override 
-		{ 
-			return TTypeId<ValueType>(); 
+		virtual uint64 Type()const override
+		{
+			return TTypeId<ValueType>();
 		}
 
-		virtual PlaceHolderBase* Clone()const override { return new PlaceHolder(Value);}
+		virtual PlaceHolderBase* Clone()const override { return new PlaceHolder(Value); }
+
+		virtual uint32 Hash()const
+		{
+			return GetTypeHash(Value);
+		}
+
+		virtual bool Equal(const PlaceHolderBase* Other)const
+		{
+			return Value == static_cast<const PlaceHolder*>(Other)->Value;
+		}
 
 	public:
 		ValueType Value;
@@ -130,3 +158,51 @@ inline T AnyCast(const FAny& Operand)
 {
 	return AnyCast<T>(const_cast<FAny&>(Operand));
 }
+
+#if ENABLE_ANY_HASHABLE
+// stop hash TFunction
+template<class FuncType>
+class FAny::PlaceHolder<TFunction<FuncType>> : public FAny::PlaceHolderBase
+{
+	using ValueType = TFunction<FuncType>;
+public:
+	PlaceHolder(ValueType InValue) :
+		Value(MoveTemp(InValue))
+	{}
+
+	virtual uint64 Type()const override
+	{
+		return TTypeId<ValueType>();
+	}
+
+	virtual PlaceHolderBase* Clone()const override { return new PlaceHolder(Value); }
+
+	virtual uint32 Hash()const
+	{
+		return 0;
+	}
+
+	virtual bool Equal(const PlaceHolderBase* Other)const
+	{
+		return false;
+	}
+
+public:
+	ValueType Value;
+
+};
+
+struct FAnyHashable
+{
+	const FAny& Any;
+	FAnyHashable(const FAny& InAny) :Any(InAny) { }
+};
+
+// avoid implicit conversition to FAny
+inline uint32 GetTypeHash(const FAnyHashable& Key)
+{
+	return Key.Any.Hash();
+}
+
+
+#endif
